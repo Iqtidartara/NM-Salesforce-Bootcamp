eb9bdb736ebc9a9b00b65d3b2d05525fa7fc6323
Iqtidartara <135112726+Iqtidartara@users.noreply.github.com>
2023-11-17 18:13:26 +0500

    new commit in acdmeytdx

--- components/Acadmey.tsx
+++ components/Acadmey.tsx
@@ -47,7 +47,7 @@ const Acadmey = () => {
             <h1 className="text-[1.25rem] leading-normal font-[600] text-[#000]">Achieve Salesforce
 Certification  </h1>
             <p className="text-[#646464] text-[1.125rem] font-[400] leading-normal">
-            Our bootcamp is designed to empower individuals with the knowledge required to successfully pass both the Salesforce Administration and Developer exams. In addition to our exceptional curriculum, Salesforce Trailheads provide the essential skills needed for exam success. Once certified, you can maintain your certifications through the upkeep modules available on Trailheads.
+            Our bootcamp is designed to empower individuals with the knowledge required to successfully pass both the Salesforce Administration and Developer exams. In addition to our exceptional curriculum&apos; Salesforce Trailheads provide the essential skills needed for exam success. Once certified&apos; you can maintain your certifications through the upkeep modules available on Trailheads.
             </p>
           </div>
 
@@ -61,7 +61,7 @@ Certification  </h1>
             />
             <h1 className="text-[1.25rem] leading-normal font-[600] text-[#000]">Elevated Job Fulfillment  <br/>Discovering </h1>
             <p className="text-[#646464] text-[1.125rem] font-[400] leading-normal">
-            Salesforce can lead to an exciting career change with great pay. In the USA, Salesforce developers earn around $140,000 on average. The cool part? You can do this job from home! So, if you're thinking about a career shift, learning Salesforce could be your ticket to a well-paying job that lets you work remotely. Ready to explore and transform your career path? Jump into Salesforce!
+            Salesforce can lead to an exciting career change with great pay. In the USA&apos; Salesforce developers earn around $140,000 on average. The cool part? You can do this job from home! So&apos; if you're thinking about a career shift&apos; learning Salesforce could be your ticket to a well-paying job that lets you work remotely. Ready to explore and transform your career path? Jump into Salesforce!
               instantly.
             </p>
           </div>
